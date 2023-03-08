ac0254d303ae3d90bfbb0367c40517e56738c4fa
camendiola <ca.mendiola@mavi.mx>
2023-03-08 10:36:31 -0600

    front LoginActivity.kt

--- app/src/main/java/com/breakapp/rev/LoginActivity.kt
+++ app/src/main/java/com/breakapp/rev/LoginActivity.kt
@@ -2,10 +2,17 @@ package com.breakapp.rev
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        try {
+            supportActionBar!!.hide()
+            actionBar!!.hide()
+        } catch (e: Exception) {
+            Log.d("CONFIGACTIVITY ", e.toString())
+        }
     }
 }
